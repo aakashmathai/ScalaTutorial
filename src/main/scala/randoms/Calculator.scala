bd5101c1235cf407a621d5d3ae14828fe18d5955
aakashmathai <aakashmathai@gmail.com>
2017-08-16 17:13:19 +0530

    Random program : Calculator added

---
+++ src/main/scala/randoms/Calculator.scala
@@ -0,0 +1,21 @@
+package randoms
+
+import java.util.Scanner
+
+object Calculator {
+  def main(args: Array[String]): Unit = {
+    print("Enter two Numbers : ")
+    val scanner = new Scanner(System.in)
+    val No1 = scanner.nextInt()
+    val No2 = scanner.nextInt()
+    print("Enter the Operation(+ * - /) : ")
+    val operator = scanner.next()
+    operator match {
+      case "+" => println("Sum is : " + (No1 + No2))
+      case "-" => println("Difference is : " + (No1 - No2))
+      case "*" => println("Product is : " + (No1 * No2))
+      case "/" => println("Quotient is : " + (No1 / No2) + "\nReminder is : " + (No1 % No2))
+      case _ => println("Enter valid operator")
+    }
+  }
+}
