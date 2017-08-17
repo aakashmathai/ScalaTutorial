1939a91c7216c7dddcd7c2c003c10286427dce7c
aakashmathai <aakashmathai@gmail.com>
2017-08-17 18:09:24 +0530

    Using Yield keyword

--- src/main/scala/chapter7/UsingYield.scala
+++ src/main/scala/chapter7/UsingYield.scala
@@ -2,26 +2,26 @@ package chapter7
 
 import java.io.File
 
-object NestedIterationsFor {
+object UsingYield {
   def main(args: Array[String]): Unit = {
-
-    //location of the files
     val filesHere = new File("/home/aakash/RandomFiles").listFiles()
 
     //adding lines in a file to a list
     def fileLines(file: java.io.File) =
       scala.io.Source.fromFile(file).getLines().toList
 
-    //method to find the lines containing a specific pattern
-    def grep(pattern: String): Unit =
+    //using yield keyword to store iteration values
+    val lineLengths =
       for {
         file <- filesHere
         if file.getName.endsWith(".scala")
         line <- fileLines(file)
         trimmed = line.trim
-        if trimmed.matches(pattern)
-      } println(file + " : " + trimmed)
+        if trimmed.matches(".*List.*")
+      } yield trimmed.length
 
-    grep(".*val .*")
+    //printing the array of lengths
+    for (i <- lineLengths.indices)
+      println(lineLengths(i))
   }
 }
