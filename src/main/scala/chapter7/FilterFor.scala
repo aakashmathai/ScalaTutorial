2546bba0dbc6e0ddfdedf083e945c26cb6f3937f
aakashmathai <aakashmathai@gmail.com>
2017-08-17 17:28:51 +0530

    Nested iterations in a For Loop

--- src/main/scala/chapter7/FilterFor.scala
+++ src/main/scala/chapter7/FilterFor.scala
@@ -1,8 +1,10 @@
 package chapter7
 
+import java.io.File
+
 object FilterFor {
   def main(args: Array[String]): Unit = {
-    val filesHere = new java.io.File("/home/aakash/RandomFiles").listFiles()
+    val filesHere = new File("/home/aakash/RandomFiles").listFiles()
     for (file <- filesHere if file.getName.endsWith(".scala"))
       println(file)
   }
