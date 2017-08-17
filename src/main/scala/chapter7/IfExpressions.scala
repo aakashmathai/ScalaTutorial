e71b20ac8733b8b413e82e9b068870bbfc065c35
aakashmathai <aakashmathai@gmail.com>
2017-08-17 13:49:24 +0530

    Chapter 7 : till while loops added

---
+++ src/main/scala/chapter7/IfExpressions.scala
@@ -0,0 +1,18 @@
+package chapter7
+
+object IfExpressions {
+  def main(args: Array[String]): Unit = {
+    println("Enter your Marks :")
+    val marks = readInt()
+    val grade =
+      if (marks >= 90) "S"
+      else if (marks >= 80) "A"
+      else if (marks >= 70) "B"
+      else if (marks >= 60) "C"
+      else if (marks >= 50) "D"
+      else if (marks >= 40) "E"
+      else "FAIL"
+    println("Grade : " + grade)
+
+  }
+}
